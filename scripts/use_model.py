e0dddccbdd127fb6c2c0c3c4a7ba8c656f468245
Arudrin <aldrinbuzz.manus@gmail.com>
2021-06-17 23:51:26 +0800

    Added comments regarding the use of load_model and model.predict

--- scripts/use_model.py
+++ scripts/use_model.py
@@ -10,13 +10,7 @@ from keras_segmentation.models.unet import mobilenet_unet
 from keras_segmentation.predict import predict
 from keras_segmentation.train import pixel_wise_loss,iou,dice
 
-postures = ['proper', 'slouch', 'techneck']
 
-preprocessed_dir = '/home/arudrin/Documents/nivfrm-ml/data/test-data'
-masks_dir ='/home/arudrin/Documents/nivfrm-ml/data/test-data-mask'
-predicted_dir = '/home/arudrin/Documents/nivfrm-ml/data/predicted-mask'
-
-val = '/home/arudrin/Documents/nivfrm-ml/val/where.jpg'
 
 enable_cuda_cache = "export CUDA_CACHE_DISABLE=0"
 set_cuda_cache    = "export CUDA_CACHE_MAXSIZE=2147483648"
@@ -32,17 +26,21 @@ session = compat.InteractiveSession(config=config)
 
 batch_size = 1
 
-# model = mobilenet_segnet(n_classes=2, input_height=240, input_width=240)
-# logs_path = "../logs/mobilenet_segnet"
+
+#FOR LOADING SAVED MODELS , GIVES VALUE ERROR, asks for custom_objects
+#model = load_model('/home/arudrin/Documents/nivfrm-ml/scripts/mobilenet_50E-0.015LR_saved_model')
 
 model = mobilenet_unet(n_classes=2, input_height=240, input_width=240, batch_size=batch_size)
 logs_path = "../logs/mobilenet_unet" 
+
 adam = optmzrs.Adam(amsgrad=True)
 model.compile(loss = pixel_wise_loss, optimizer = adam, metrics = [iou, dice])
 
 model.load_weights('/home/arudrin/Documents/nivfrm-ml/logs/mobilenet_unet_50e-0.015LR/checkpoints/weights-050-0.5345.hdf5')
 
+#val = inference result, inp = input
+val = '/home/arudrin/Documents/nivfrm-ml/val/where.jpg'
 inp = '/home/arudrin/Documents/nivfrm-ml/scripts/data_in_scriptfolder/raw-240x240/proper/1.jpg' 
-predict(model,inp,val)
+predict(model,inp,val) #modified model.predict from /keras_segmentation/predict.py
 	
 
